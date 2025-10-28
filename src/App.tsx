f8491b68baff86766fb72f83a20ffa85df03d0bb
Darkø Tasevski <darkota@payoneer.com>
2025-10-28 09:40:55 +0100

    feat: add honeycomb integration

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,34 @@ import Header from "@/components/global/Header";
 import ChooseFlow from "@/pages/ChooseFlow";
 import Checkout from "@/features/embeddedCheckout/pages/Checkout";
 import HostedCheckout from "@/features/hostedCheckout/pages/HostedCheckout";
+import { getWebAutoInstrumentations } from "@opentelemetry/auto-instrumentations-web";
+import { HoneycombWebSDK } from "@honeycombio/opentelemetry-web";
+import { useLayoutEffect } from "react";
+
+const configDefaults = {
+  ignoreNetworkEvents: true,
+  // propagateTraceHeaderCorsUrls: [
+  // /.+/g, // Regex to match your backend URLs. Update to the domains you wish to include.
+  // ]
+};
 
 function App() {
+  useLayoutEffect(() => {
+    const sdk = new HoneycombWebSDK({
+      // endpoint: "otel-collector.dev.oscato.com", // crashing the app  if uncommented (Configuration: Could not parse user-provided export URL: 'otel-collector.dev.oscato.com/v1/traces')
+      debug: true,
+      serviceName: "hosted_v6",
+      instrumentations: [
+        getWebAutoInstrumentations({
+          "@opentelemetry/instrumentation-xml-http-request": configDefaults,
+          "@opentelemetry/instrumentation-fetch": configDefaults,
+          "@opentelemetry/instrumentation-document-load": configDefaults,
+        }),
+      ],
+    });
+    sdk.start();
+  }, []);
+
   return (
     <Router basename="/">
       <div className="min-h-screen bg-white dark:bg-gray-800">
